fe3a877c2a73794b8e147fe69498eb01cd2f727c
Danny Vasta <djv711@gmail.com>
2024-11-24 14:15:00 -0500

    tweak some styling and fixed display jump from 00 seconds to -1 to 59

--- src/components/timer/input.tsx
+++ src/components/timer/input.tsx
@@ -5,18 +5,19 @@ interface InputProps extends ComponentPropsWithoutRef<"input"> {
   value: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
   onBlur: ChangeEventHandler<HTMLInputElement>;
+  cn?: string;
 }
 
-const Input = ({ id, value, onChange, onBlur, ...rest }: InputProps) => (
+const Input = ({ id, value, onChange, onBlur, cn, ...rest }: InputProps) => (
   <input
     type="text"
     pattern="[0-9]"
     maxLength={2}
     id={id}
-    value={value}
+    value={+value < 0 ? "00" : value}
     onChange={onChange}
     onBlur={onBlur}
-    className="max-w-[2ch] text-card-foreground bg-card"
+    className={`max-w-[2ch] text-card-foreground bg-card text-center ${cn}`}
     {...rest}
   />
 );
