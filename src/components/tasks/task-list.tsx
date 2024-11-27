d48ab092fc960747d59c28b03915408cd335ee6f
Danny Vasta <djv711@gmail.com>
2024-11-27 13:33:42 -0500

    update add task transparency issue. add auto scroll to top when starting timer

--- src/components/tasks/task-list.tsx
+++ src/components/tasks/task-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import {
   Card,
   CardHeader,
@@ -14,9 +14,11 @@ import EditableTask from "./editable-task";
 import { Plus } from "lucide-react";
 import { Task } from "./types";
 import { TimerContext } from "@/components/timer-provider";
+import usePrefersReducedMotion from "@/app/hooks/use-prefers-reduced-motion";
 
 const TaskList = () => {
   const { running, onBreak } = useContext(TimerContext);
+  const prefersReducedMotion = usePrefersReducedMotion();
   const [newTaskText, setNewTaskText] = useState<string>("");
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, text: "first task", status: "todo" },
@@ -27,6 +29,20 @@ const TaskList = () => {
       status: "completed",
     },
   ]);
+
+  // abandon in-progress edits if the timer starts
+  useEffect(() => {
+    if (running) {
+      setNewTaskText("");
+      document.body.style.overflow = "hidden";
+      window.scrollTo({
+        top: 0,
+        behavior: prefersReducedMotion ? "auto" : "smooth",
+      });
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [running]);
 
   const addTask = (text: string) => {
     setTasks((tasks) => [...tasks, { id: Date.now(), text, status: "todo" }]);
@@ -50,6 +66,7 @@ const TaskList = () => {
 
   return (
     <Card
+      id="task-list"
       className={`w-full shadow-lg z-0 transition-colors duration-300 ${
         running && "border-transparent shadow-none text-transparent select-none"
       } ${onBreak && "bg-transparent"}`}
@@ -57,9 +74,7 @@ const TaskList = () => {
       <CardHeader>
         <CardTitle>Tasks</CardTitle>
       </CardHeader>
-      <CardContent
-        className={`flex flex-col gap-y-2 ${running && "text-transparent"}`}
-      >
+      <CardContent className="flex flex-col gap-y-2">
         <ul className="flex flex-col gap-y-2">
           {tasks.map((task) => (
             <EditableTask
@@ -76,9 +91,8 @@ const TaskList = () => {
             id="add-new-task-input"
             aria-label="add new task input"
             value={newTaskText}
-            className={`border-dashed py-2 text-center transition-opacity transition-opacity duration-300 ${
-              running &&
-              "placeholder:text-transparent text-transparent border-transparent bg-transparent"
+            className={`border-dashed py-2 text-center transition-opacity duration-300 ${
+              running && "!opacity-0"
             }`}
             placeholder="add task"
             onChange={(e) => setNewTaskText(e.target.value)}
@@ -87,6 +101,9 @@ const TaskList = () => {
                 addTask(newTaskText.trim());
                 setNewTaskText("");
                 document?.getElementById("add-new-task-input")?.blur();
+                document?.getElementById("task-list")?.scrollIntoView({
+                  behavior: prefersReducedMotion ? "auto" : "smooth",
+                });
               }
             }}
             disabled={running}
@@ -95,9 +112,8 @@ const TaskList = () => {
             id="add-new-task-button"
             aria-label="add new task button"
             variant="outline"
-            className={`w-fit px-6 aria-disabled:border-dashed transition-opacity duration-300 ${
-              running &&
-              "placeholder:text-transparent text-transparent border-transparent bg-transparent"
+            className={`w-fit px-6 transition-opacity duration-300 ${
+              running && "!opacity-0"
             }`}
             onClick={() => {
               addTask(newTaskText.trim());
